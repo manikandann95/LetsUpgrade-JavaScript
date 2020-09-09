b92403d659232a5a0b3b0dbad6f1855660a43b3e
Manikandan N <manikandann95@users.noreply.github.com>
2020-09-09 23:11:23 +0530

    Add files via upload

---
+++ Day 4 Assignments/index3.js
@@ -0,0 +1,31 @@
+let students=[ 
+ {
+ name : "manik", 
+ age : 25, 
+ country : "India",
+ hobbies : ["cards","videogames","movies"],
+ },
+ {
+ name : "shyam", 
+ age : 18, 
+ country : "Australia",
+ hobbies : ["golf","cricket","drawing"],
+ },
+ {
+ name : "daniel", 
+ age : 22, 
+ country : "USA",
+ hobbies : ["rugby","skating","dancing"],
+ },
+ {
+ name : "gabriel", 
+ age : 19,
+ country : "Canada",
+ hobbies : ["icehockey","pub","singing"],
+ },
+ 	]
+ function disp(stud){
+ 	console.log(stud);	
+ 		 } 
+disp(students);
+			
