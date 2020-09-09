ee61a6b609f75baf7d208209dc4c62a118b026ca
Manikandan N <manikandann95@users.noreply.github.com>
2020-09-09 22:21:14 +0530

    Add files via upload
    Day 4 Assignment question 1

---
+++ Day 4 Assignments/index1.js
@@ -0,0 +1,18 @@
+function changeImage1(){
+	const ele= document.getElementsByClassName("imgmanik");
+	console.log(ele[0].class)
+	const newURL="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gardenia-royalty-free-image-1580854928.jpg";
+	ele[0].src=newURL;
+	}	
+
+function changeImage2(){
+	const ele= document.getElementsByClassName("imgmanik");
+	const newURL="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-blossoming-rose-flower-royalty-free-image-1580853844.jpg";
+	ele[0].src=newURL;
+	}
+
+function changeImage3(){
+	const ele= document.getElementsByClassName("imgmanik");
+	const newURL="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/petunias-royalty-free-image-1580855855.jpg";
+	ele[0].src=newURL;
+	}
